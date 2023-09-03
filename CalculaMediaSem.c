15eb848b324ec2e7c62e91636b7222fd51247e71
Izabella Aro <izabella.aro20@gmail.com>
2023-09-03 19:02:57 -0300

    altera nomes de variavel e formatacao de codigo

--- CalculaMediaSem.c
+++ CalculaMediaSem.c
@@ -1,37 +1,37 @@
 int main(void)
 {
-    float np1, np2, pim, media, pExame, mExame;
+    float notaProva1, notaProva2, pim, media, notaExame, mediaExame;
 
     printf("***Calculadora de Medias***\n");
 
-    printf("\nDigite a nota da sua NP1: ");
-    scanf("%f", &np1);
-    printf("Digite a nota da sua NP2: ");
-    scanf("%f", &np2);
+    printf("\nDigite a nota da sua notaProva1: ");
+    scanf("%f", &notaProva1);
+
+    printf("Digite a nota da sua notaProva2: ");
+    scanf("%f", &notaProva2);
+
     printf("Digite a nota do seu PIM: ") ;
     scanf("%f", &pim);
 
-    media = (np1*0.4+np2*0.4+pim*0.2);
+    media = (notaProva1 * 0.4 + notaProva2 * 0.4 + pim * 0.2);
 
     if(media >= 7 ){
         printf("\nAluno Aprovado!  Parabens :) ");
-        printf("\nSua media e: ");
-        printf("%.1f", media);
+        printf("\nSua media e: %0.1f", media);
+        
     }
     else
     {
         printf("\nAluno devera realizar o Exame \n");
 
         printf("\nDigite a nota do seu Exame: ");
+        scanf("%f", &notaExame);
 
-        scanf("%f", &pExame);
+        mediaExame = (media + notaExame) / 2;
 
-        mExame = (media+pExame)/2;
-
-        if(mExame>=5){
+        if(mediaExame >= 5){
             printf("\nAluno Aprovado! Parabens :)\n");
-            printf("\nSua media e: ");
-            printf("%.1f", mExame);
+            printf("\nSua media e: %0.1f", mediaExame);
         }
         else
         {
